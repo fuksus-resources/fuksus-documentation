8bd8b3158713ef59b9bd9d7e02743c296ddc51a5
boostless <46201063+boostless@users.noreply.github.com>
2025-01-27 15:33:23 +0200

    initial structure (#2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,18 @@ export default function Home() {
   return (
     <div className="flex sm:min-h-[91vh] min-h-[88vh] flex-col items-center justify-center text-center px-2 py-8">
       <Link
-        href="https://github.com/nisabmohd/Aria-Docs"
+        href="https://resources.fuksus.com/"
         target="_blank"
         className="mb-5 sm:text-lg flex items-center gap-2 underline underline-offset-4"
       >
-        Follow along on GitHub{" "}
+        Check out our store at{" "}
         <MoveUpRightIcon className="w-4 h-4 font-extrabold" />
       </Link>
       <h1 className="text-3xl font-bold mb-4 sm:text-7xl">
-        An example app built using Next.js and server components.
+        Welcome to Fuksus Resources documentation!
       </h1>
       <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
-        This feature-packed documentation template, built with Next.js, offers a
-        sleek and responsive design, perfect for all your project documentation
-        needs.
+        At this documentation file you can search for guidelines, error fixes and find answers to other questions about Fuksus Resouces scripts.
       </p>
       <div className="flex flex-row items-center gap-5">
         <Link
@@ -29,21 +27,7 @@ export default function Home() {
         >
           Get Stared
         </Link>
-        <Link
-          href="/blog"
-          className={buttonVariants({
-            variant: "secondary",
-            className: "px-6",
-            size: "lg",
-          })}
-        >
-          Read Blog
-        </Link>
       </div>
-      <span className="flex flex-row items-start sm:gap-2 gap-0.5 text-muted-foreground text-md mt-7 -mb-12 max-[800px]:mb-12 font-code text-base font-medium">
-        <TerminalSquareIcon className="w-5 h-5 mr-1 mt-0.5" />
-        {"npx create-aria-doc <project-directory>"}
-      </span>
     </div>
   );
 }
